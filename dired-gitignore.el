44f2ad9c5227a41d93359be394bb629ede1439f8
Johannes Mueller <github@johannes-mueller.org>
2023-04-08 11:37:08 +0200

    Upgrade to GPL-3 in order to make it fit for MELPA

--- dired-gitignore.el
+++ dired-gitignore.el
@@ -10,7 +10,7 @@
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation version 2. <https://www.gnu.org/licenses/>
+;; the Free Software Foundation version 3. <https://www.gnu.org/licenses/>
 
 ;; This package is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
