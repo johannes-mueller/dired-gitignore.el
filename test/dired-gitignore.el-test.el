a4b67066f2d901e79ee13aa533c39b1961e76685
Johannes Mueller <github@johannes-mueller.org>
2023-04-23 22:03:57 +0200

    Require ls-lisp

--- test/dired-gitignore.el-test.el
+++ test/dired-gitignore.el-test.el
@@ -39,6 +39,7 @@
    (message "last line %s" (thing-at-point 'line t))
    (message "complete buffer\n%s\n" (buffer-string))
    (message "dired-listing-switches: %s" dired-listing-switches)
+   (require 'ls-lisp)
    (message "ls-lisp-use-insert-directory-program: %s" ls-lisp-use-insert-directory-program)
    (should (eq (count-lines (point-min) (point-max)) 8))))
 
