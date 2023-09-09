09e620e69c660f1468b2652832ff579512cc0f47
Johannes Mueller <github@johannes-mueller.org>
2023-09-09 12:08:06 +0200

    Introduce dired-gitignore-global-mode (#15)
    * Introduce dired-gitignore-global-mode

--- dired-gitignore.el
+++ dired-gitignore.el
@@ -23,13 +23,18 @@
 ;; ignored by git due to an entry in the project's `.gitignore'.
 
 ;; When the minor mode is active, the ignored items are hidden.  When it is
-;; deactivated, they are shown again.  The recommended use case is to bind the
-;; command to toggle the minor mode `(dired-gitignore-mode)' to some
-;; convenient key.
+;; deactivated, they are shown again.
 
-;; In order to hide ignored files by default hook it into `dired-mode-hook'
+;; The recommended use case is to bind the command to toggle the global minor mode
+;; `(dired-gitignore-global-mode)' to some convenient key.  If you want to ignore
+;; .gitignored files by default put
 ;;
-;; (add-hook 'dired-mode-hook 'dired-gitignore-mode)
+;; (dired-gitignore-global-mode t)
+;;
+;; into your startup file.
+
+;; Alternatively you can to use `(dired-gitignore-mode)' to toggle the ignoring.
+;; That affects only the current `dired' buffer.
 
 ;; It needs the executables for `git' and `ls' in the `PATH' and a standard UNIX shell
 ;; behind the `shell-file-name' variable.  The fish shell <3.4.0 does not work.
@@ -37,6 +42,8 @@
 ;;; Code:
 
 (require 'dired)
+
+(defvar dired-gitignore--global-mode-active nil)
 
 ;;;###autoload
 (define-minor-mode dired-gitignore-mode
@@ -49,6 +56,20 @@
     (remove-hook 'dired-after-readin-hook #'dired-gitignore--hide))
   (revert-buffer))
 
+;;;###autoload
+(defun dired-gitignore-global-mode (&optional yn)
+  "Toggle, enable or disable dired-gitignore-mode globally.
+Without argument the global dired-gitignore-mode is toggled.  If
+YN is -1 it is disabled, if it is non-nil it is enabled."
+  (interactive)
+  (cond
+   ((eq yn nil) (setq dired-gitignore--global-mode-active (not dired-gitignore--global-mode-active)))
+   ((eq yn -1) (setq dired-gitignore--global-mode-active nil))
+   (t (setq dired-gitignore--global-mode-active t)))
+  (when (derived-mode-p 'dired-mode)
+    (if dired-gitignore--global-mode-active
+        (dired-gitignore-mode t)
+      (dired-gitignore-mode -1))))
 
 (defun dired-gitignore--hide ()
   "Determine the lines to be hidden and hide them."
@@ -61,7 +82,6 @@
       (dired-do-kill-lines nil "")
       (dired-gitignore--restore-marks marked-files))))
 
-
 (defun dired-gitignore--remove-all-marks ()
   "Remove all marks and return the marks removed."
   (save-excursion
@@ -70,12 +90,10 @@
       (while (re-search-forward dired-re-mark nil t)
         (subst-char-in-region (1- (point)) (point) (preceding-char) ?\s)))))
 
-
 (defun dired-gitignore--files-to-be-ignored ()
   "Determine and return a list of files to be ignored."
   (split-string
    (shell-command-to-string "git ls-files -oi --exclude-standard --directory")))
-
 
 (defun dired-gitignore--mark-file (file)
   "Mark the file FILE in the Dired buffer."
@@ -85,13 +103,17 @@
       (dired-mark 1))
       absolute-file))
 
-
 (defun dired-gitignore--restore-marks (marked-files)
   "Restore the marks of MARKED-FILES."
   (dolist (file marked-files)
         (dired-goto-file file)
         (dired-mark 1)))
 
+(defun dired-gitignore--turn-on-if-globally-activated ()
+  (when dired-gitignore--global-mode-active
+    (dired-gitignore-mode)))
+
+(add-hook 'dired-mode-hook #'dired-gitignore--turn-on-if-globally-activated)
 
 (provide 'dired-gitignore)
 ;;; dired-gitignore.el ends here
